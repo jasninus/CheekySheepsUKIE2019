a73e567fbbf8550334cb8a73f2a716f66531de4c
loguntetley <loguntetley@outlook.com>
2019-11-05 20:46:40 +0000

    Credits Scene

--- CheekySheepsUKIE2019/Assets/MainMenu.cs
+++ CheekySheepsUKIE2019/Assets/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
     //gets the next scene in the que so it will load the level
     public void PlayButton() { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
 
+    public void CreditsButton() { SceneManager.LoadScene("Credits"); }
+
     public void QuitButton()
     {
         Debug.Log("Quite Button is working");

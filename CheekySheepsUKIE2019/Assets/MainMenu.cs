ee61e064bcd658cab7cbcaa213086d5e7c20b06d
loguntetley <loguntetley@outlook.com>
2019-11-05 17:30:21 +0000

    Audio & Menu systems

---
+++ CheekySheepsUKIE2019/Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //gets the next scene in the que so it will load the level
+    public void PlayButton() { SceneManager.LoadScene(/*SceneManager.GetActiveScene().buildIndex + */1); }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quite Button is working");
+        Application.Quit();
+    }
+}

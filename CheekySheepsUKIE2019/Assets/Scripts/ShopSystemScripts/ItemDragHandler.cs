920c3792d44e8dced37310de7c91b7b380f3bbbf
DarudE <w1682416@my.westminster.ac.uk>
2019-11-05 16:17:42 +0000

    RayCasting

---
+++ CheekySheepsUKIE2019/Assets/Scripts/ShopSystemScripts/ItemDragHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    private Vector2 rectTransform;
+    private RectTransform rectTrans;
+
+    private void Awake()
+    {
+        rectTrans = GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //transform.position = rectTransform;
+        rectTrans.anchoredPosition = rectTransform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GameObject.Find("ItemImage").GetComponent<RectTransform>().anchoredPosition;
+    }
+
+}

0b85d6c44403dfad9f7dd598f5899e874a646b2e
jasninus <jasninus8@hotmail.dk>
2019-11-05 13:33:23 +0100

    Volunteer framework

---
+++ CheekySheepsUKIE2019/Assets/Scripts/Volunteer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volunteer : MonoBehaviour
+{
+    private VolunteerType type;
+
+    public void OpenInfoPanel()
+    {
+        type.GetInfoUI().SetActive(true);
+    }
+
+    public void CloseInfoPanelCanvas()
+    {
+        type.GetInfoUI().SetActive(false);
+    }
+
+    public void UpgradeTo(VolunteerRole newRole)
+    {
+        switch (newRole)
+        {
+            case VolunteerRole.FireExtinguisher:
+                type = GetComponent<FireExtinguisherVolunteer>();
+                break;
+
+            case VolunteerRole.AnimalSaver:
+                type = GetComponent<AnimalSaverVolunteer>();
+                break;
+
+            case VolunteerRole.Planter:
+                type = GetComponent<PlanterVolunteer>();
+                break;
+        }
+    }
+}
+
+public enum VolunteerRole
+{
+    FireExtinguisher,
+    Planter,
+    AnimalSaver,
+    WaterGatherer
+}

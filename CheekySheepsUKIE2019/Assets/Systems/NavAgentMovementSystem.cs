5b68561436d56f57c3a8ab867d9289d9d789b395
Ayran Olckers <w1654684@my.westminster.ac.uk>
2019-11-05 15:21:10 +0000

    ECS implementation -
    an entity is just an id, which sole purpose is to group components logically together.
    
    a component is just a store for data (a ‘struct’ without code logic).
    
    a system is responsible for processing / transforming the data stored in the components; to achieve this, it iterates over homogeneous groups of components.

---
+++ CheekySheepsUKIE2019/Assets/Systems/NavAgentMovementSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class NavAgentMovementSystem : JobComponentSystem
+{
+    public struct NavAgentMovementJob : IJobForEach<Translation, UnitNavAgent>
+    {
+        public float dT;
+
+        public void Execute(ref Translation position, [ReadOnly] ref UnitNavAgent agent)
+        {
+            float distance = math.distance(agent.finalDestination, position.Value);
+            float3 direction = math.normalize(agent.finalDestination - position.Value);
+            float speed = 5;
+            if(!(distance < 0.5) && agent.agentStatus == NavAgentStatus.Moving)
+            {
+                position.Value += direction * speed * dT;
+            }
+        }
+    }
+
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new NavAgentMovementJob
+        {
+            dT = Time.deltaTime,
+        };
+        return job.Schedule(this, inputDeps);
+    }
+}
---
+++ CheekySheepsUKIE2019/Assets/Systems/PlayerUnitMovementSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class PlayerUnitMovementSystem : JobComponentSystem
+{
+
+    public struct PlayerUnitMovementJob : IJobForEach<PlayerInput, UnitNavAgent, PlayerUnitSelect>
+    {
+        public float dT;
+        public float3 mousePos;
+
+        public void Execute
+            ([ReadOnly] ref PlayerInput pInput, ref UnitNavAgent navAgent, [ReadOnly] ref PlayerUnitSelect selected)
+        {
+           if (pInput.RightClick)
+            {
+                navAgent.finalDestination = mousePos;
+                navAgent.agentStatus = NavAgentStatus.Moving;
+
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if(hit.collider != null)
+            {
+                mousePos = new float3(hit.point.x, 0, hit.point.z);
+            }
+        }
+        var job = new PlayerUnitMovementJob
+        {
+            mousePos = mousePos
+        };
+        return job.Schedule(this, inputDeps);
+    }
+}

b6346355ca6e8a0a0fff3cb8bd9006e82c0ac664
loguntetley <loguntetley@outlook.com>
2019-11-06 14:41:31 +0000

    SocialMedia popUp done

--- CheekySheepsUKIE2019/Assets/PopUpBox.cs
+++ CheekySheepsUKIE2019/Assets/PopUpBox.cs
@@ -14,6 +14,7 @@ public class PopUpBox : MonoBehaviour
 
     //public static bool PopUpState = false;
     public GameObject PopUpBoxes;
+    public CanvasGroup PopUpBoxes1;
     public Image ImagetoSet;
     public Sprite Armadillo;
     public Sprite Cat;
@@ -25,34 +26,38 @@ public class PopUpBox : MonoBehaviour
         //for testing only as I don't know the requirements
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PicPicker();
+            PicPicker(3);
+           
             PopUpActivation();
+            //FadeOut();
         }
     }
 
     void PopUpActivation()
     {
-        PopUpBoxes.SetActive(true);
-        float secondsLeft = 0;
-
-        void Start() { StartCoroutine(DelayLoadLevel(4)); }
-        Start();
-        IEnumerator DelayLoadLevel(float seconds)
-        {
-            secondsLeft = seconds;
-            do { yield return new WaitForSeconds(1); }
-            while (--secondsLeft > 0);
-            PopUpBoxes.SetActive(false);
-        }
+        //PopUpBoxes.SetActive(true);
+        //float secondsLeft = 0;
+        //
+        //void Start() { StartCoroutine(DelayLoadLevel(4)); }
+        //Start();
+        //IEnumerator DelayLoadLevel(float seconds)
+        //{
+        //    secondsLeft = seconds;
+        //    do { yield return new WaitForSeconds(1); }
+        //    while (--secondsLeft > 0);
+        //    PopUpBoxes.SetActive(false);
+        //}
+        FadeIn(PopUpBoxes1);
+        StartCoroutine(DelayAnimation(PopUpBoxes1));
     }
 
     //changing dependant on what triggers each animal
-     int AniNum = 0;
+     
 
-    void PicPicker ()
+    void PicPicker (int AnimalIndex)
     {
-        int AniNum = 0;
-        switch (AniNum)
+        
+        switch (AnimalIndex)
         {
             case 0:
                 ImagetoSet.sprite = Armadillo;
@@ -71,4 +76,88 @@ public class PopUpBox : MonoBehaviour
                 break;
         }
     }
+    /*
+     using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AwarenessButton : MonoBehaviour
+{
+    [SerializeField] private Button awarenessButton;
+
+    [SerializeField] private CanvasGroup[] uiElements;
+
+    private uint PopUpIndex;
+
+
+    //if you click a button spawn a pop up;
+    //increase the index for the pop ups;
+    //disable the button for a couple of seconds
+    //increase the awareness points
+    //instanciate new social media pop up
+    //have a delay and then start fade out the pop up 
+    // eventually move the pop up up 
+
+
+    public void OnPressedButton()
+    {
+        //awarenesspoints+=200;
+        awarenessButton.interactable = false;
+        PopUpAnimation(uiElements[Random.Range(0, 15)]);
+        StartCoroutine(WhateverYouWant());
+
+    }
+
+    IEnumerator WhateverYouWant ()
+    {
+        yield return new WaitForSeconds(15);
+        awarenessButton.interactable = true;
+    }*/
+
+
+    public void FadeOut(CanvasGroup PopUp)
+    {
+        StartCoroutine(FadePopUp(PopUp, PopUp.alpha, 0));
+    }
+    public void FadeIn(CanvasGroup PopUp)
+    {
+        StartCoroutine(FadePopUp(PopUp, PopUp.alpha, 1));
+    }
+    public IEnumerator FadePopUp(CanvasGroup cg, float start, float end, float lerpTime = 1f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float PercentageComplete = timeSinceStarted / lerpTime;
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            PercentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, PercentageComplete);
+            cg.alpha = currentValue;
+
+            if (PercentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+    
+   
+    public void PopUpAnimation(CanvasGroup PopUp)
+    {
+
+        FadeIn(PopUp);
+        StartCoroutine(DelayAnimation(PopUp));
+
+    }
+
+    public IEnumerator DelayAnimation(CanvasGroup PopUp)
+    {
+        yield return new WaitForSeconds(3);
+        FadeOut(PopUp);
+    }
+   
 }
+

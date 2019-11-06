3193fc094d3b82daeec39bb5cd68dd19a89d0d55
Darudens <45597290+Darudens@users.noreply.github.com>
2019-11-06 12:57:30 +0000

    MusicManagerCreated

---
+++ CheekySheepsUKIE2019/Assets/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = FindObjectOfType<AudioSource>();
+        AudioSource.loop = false;
+    }
+
+    private AudioClip RandomClip()
+    {
+        return clips[Random.Range(0, clips.Length)];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!AudioSource.isPlaying)
+        {
+            AudioSource.clip = RandomClip();
+            AudioSource.Play();
+        }
+    }
+}
